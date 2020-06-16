97a26fe7b0d265c80082914502211b5e2b03325f
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-16 23:29:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,20 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costumes
+      t.integer :employees
+      t.string :active
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+end
+  
+  
+  
+  
+  
+  
+  # Create your costume_stores migration here
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
-# Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.string :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :description
+    end
+  end
+end
+  
+  # Create your haunted_houses migration here
